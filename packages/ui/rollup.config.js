5027f29e7bda7d6e3c0d62c92730d6f83d31a513
ycjcl868 <chaolinjin@gmail.com>
2021-12-19 22:00:49 +0800

    feat: vite app

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'rollup';
 import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss'
+import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
+import vue from 'rollup-plugin-vue';
 import { babel } from '@rollup/plugin-babel';
 import { name } from './package.json';
 
@@ -9,8 +12,8 @@ export default defineConfig([
     input: 'react/index.tsx',
     external: ['react', 'react-dom'],
     plugins: [
-      commonjs(),
       typescript(),
+      postcss({}),
       babel({
         babelrc: false,
         exclude: '**/node_modules/**',
@@ -30,7 +33,8 @@ export default defineConfig([
             },
           ],
         ],
-      })
+      }),
+      commonjs(),
     ],
     output: [
       {
@@ -50,6 +54,58 @@ export default defineConfig([
       {
         name,
         file: './lib/react/index.js',
+        format: 'commonjs',
+      }
+    ]
+  },
+  {
+    input: 'vue/index.ts',
+    external: ['vue'],
+    plugins: [
+      typescript(),
+      resolve({
+        extensions: ['.ts', '.js', '.vue'],
+      }),
+      postcss({}),
+      vue(),
+      babel({
+        presets: ['@babel/preset-env'],
+        plugins: [
+          '@babel/plugin-proposal-object-rest-spread',
+          '@babel/plugin-syntax-object-rest-spread',
+          [
+            '@babel/plugin-transform-runtime',
+            {
+              absoluteRuntime: false,
+              corejs: false,
+              helpers: false,
+              regenerator: true,
+              useESModules: false,
+            },
+          ],
+        ],
+        babelrc: false,
+        extensions: ['.js', '.ts', '.tsx', '.jsx', '.es6', '.es', '.mjs', '.vue'],
+      }),
+      commonjs(),
+    ],
+    output: [
+      {
+        name,
+        file: './dist/vue/index.js',
+        format: 'umd',
+        globals: {
+          vue: 'vue',
+        }
+      },
+      {
+        name,
+        file: './es/vue/index.js',
+        format: 'es',
+      },
+      {
+        name,
+        file: './lib/vue/index.js',
         format: 'commonjs',
       }
     ]
---
+++ packages/ui/vue/Component.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 1,
+    }
+  },
+  methods: {
+    increment() {
+      // `this` will refer to the component instance
+      this.count++
+    }
+  }
+}
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <p>{{ count }}</p>
+  <button @click="increment">Add</button>
+</template>
