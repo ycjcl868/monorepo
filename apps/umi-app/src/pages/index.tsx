61eae07d8bd28b4b89effc1d866da17f21f38c60
ycjcl868 <chaolinjin@gmail.com>
2021-12-21 15:12:06 +0800

    chore: support webpack 4

--- apps/umi-app/src/pages/index.tsx
+++ apps/umi-app/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import styles from './index.less';
+import { AppType } from '@infras/shared/types';
 import { Component } from '@infras/ui/react';
 
 export default function IndexPage() {
   return (
     <div>
+      <p>AppType.Web: {AppType.Web}</p>
       <h1 className={styles.title}>Page index</h1>
       <Component />
     </div>
