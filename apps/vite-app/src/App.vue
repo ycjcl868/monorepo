39eef5f04c8bae630ea9ebb67042a728d0202c21
ycjcl868 <chaolinjin@gmail.com>
2021-12-21 23:37:34 +0800

    fix: vite app not using injected

--- apps/vite-app/src/App.vue
+++ apps/vite-app/src/App.vue
@@ -2,11 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import { AppType } from '@infras/shared/types';
+import { Component } from '@infras/ui/vue';
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <p>AppType.Web: {{AppType.Web}}</p>
+  <Component />
 </template>
 
 <style>
---
+++ apps/vite-react-app/src/App.tsx
@@ -0,0 +1,21 @@
+import { AppType } from '@infras/shared/types';
+import { sum } from '@infras/shared/utils';
+import { Component } from '@infras/ui/react';
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>AppType.Web：{AppType.Web}</p>
+        <p>sum(1, 1): {sum(1, 1)}</p>
+        <Component />
+      </header>
+    </div>
+  )
+}
+
+export default App
