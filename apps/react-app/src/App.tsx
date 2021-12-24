d58a150c31bcf6ec00027000c6c2a8390563da64
ycjcl868 <chaolinjin@gmail.com>
2021-12-24 13:40:42 +0800

    chore: title

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>Create React App</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <Component />
         <p>
