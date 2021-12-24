f42f56a4d3fcf7216c80ed60ba5aed4e9b8ae025
ycjcl868 <chaolinjin@gmail.com>
2021-12-24 08:50:34 +0800

    chore: esm call

---
+++ apps/node-app/index.mjs
@@ -0,0 +1,9 @@
+import { sum } from '@infras/shared/utils';
+import { sum as rsSum } from '@infras/native';
+
+console.time("Node.js time");
+console.log('Node.js \`sum(1, 1)\`:', sum(1, 1));
+console.timeEnd("Node.js time");
+console.time("Rust time");
+console.log('Rust \`sum(1, 1)\`:', rsSum(1, 1));
+console.timeEnd("Rust time");
