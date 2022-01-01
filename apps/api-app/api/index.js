6bb0ae15128143f54a14e44c780060ca77390c75
ycjcl868 <chaolinjin@gmail.com>
2022-01-01 12:02:48 +0800

    chore: api

--- apps/api-app/api/index.js
+++ apps/api-app/api/index.js
@@ -4,14 +4,14 @@ const { Component } = require('@infras/ui/react');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 
-module.exports = (req, res) => {
+export default function handler(req, res) {
   let resp = '';
 
   resp += `<div>Node.js \`sum(1, 1)\`: ${sum(1, 1)}</div>`;
   resp += `<div>Rust \`sum(1, 1)\`: ${rsSum(1, 1)}</div>`
   resp += `<div>SSR: ${ReactDOMServer.renderToString(React.createElement(Component))}</div>`
 
-  res.send(`
+  res.status(200).send(`
     <html>
       <body>
         ${resp}
