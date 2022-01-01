092d0b43f72d6a1f432685357b7565741f932154
ycjcl868 <chaolinjin@gmail.com>
2022-01-01 11:51:39 +0800

    chore: api-app

--- apps/api-app/api/index.js
+++ apps/api-app/api/index.js
@@ -1,12 +1,10 @@
-const express = require('express');
 const { sum } = require('@infras/shared/utils');
 const { sum: rsSum } = require('@infras/native');
 const { Component } = require('@infras/ui/react');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 
-const app = express();
-app.use('/', (_req, res) => {
+module.exports = (req, res) => {
   let resp = '';
 
   resp += `<div>Node.js \`sum(1, 1)\`: ${sum(1, 1)}</div>`;
@@ -20,7 +18,4 @@ app.use('/', (_req, res) => {
       </body>
     </html>
   `);
-})
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server in running in port ${PORT}`));
+}
