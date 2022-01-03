dff1397537919319037adfb6642929e7b4da1993
ycjcl868 <chaolinjin@gmail.com>
2022-01-03 12:30:44 +0800

    feat: prettier

--- apps/vite-app/vite.config.ts
+++ apps/vite-app/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import { viteExternalsPlugin } from 'vite-plugin-externals'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    viteExternalsPlugin({
+      vue: 'Vue'
+    })
+  ]
 })
