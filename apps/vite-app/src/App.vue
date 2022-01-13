d898bcec42b0f8a2ecd9947b0e82cd121fcc260d
ycjcl868 <chaolinjin@gmail.com>
2022-01-13 19:27:42 +0800

    feat: add vue3 cli (#3)
    * feat: add vue-2-cli
    
    * feat: vue 3 cli
    
    * chore: update pkg

--- apps/vite-app/src/App.vue
+++ apps/vite-app/src/App.vue
@@ -2,14 +2,16 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import { AppType } from '@infras/shared/types';
-import { Component } from '@infras/ui/vue';
+import { SharedComponent } from '@infras/ui/vue';
 </script>
 
 <template>
-  <h2>Vue Vite App</h2>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <p>AppType.Web: {{AppType.Web}}</p>
-  <Component />
+  <div>
+    <h2>Vue Vite App</h2>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <p>AppType.Web: {{AppType.Web}}</p>
+    <SharedComponent />
+  </div>
 </template>
 
 <style>
---
+++ apps/vue3-cli-app/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h2>Vue cli 3 </h2>
+    <p>AppType.Web: {{AppType.Web}}</p>
+    <SharedComponent />
+  </div>
+</template>
+
+<script>
+import { AppType } from '@infras/shared/types';
+import { SharedComponent } from '@infras/ui/vue';
+
+export default {
+  name: 'App',
+  components: {
+    SharedComponent
+  },
+  data() {
+    return {
+      AppType
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
