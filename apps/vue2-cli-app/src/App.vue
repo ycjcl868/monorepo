0b6dfe1ca4dc0fc861612f50168646a9a2800b2f
ycjcl868 <chaolinjin@gmail.com>
2022-01-13 17:52:44 +0800

    feat: add vue-2-cli

---
+++ apps/vue2-cli-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <p>AppType.Web: {{AppType.Web}}</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { AppType } from '@infras/shared/types';
+// import { Component } from '@infras/ui/vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    // Component,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
