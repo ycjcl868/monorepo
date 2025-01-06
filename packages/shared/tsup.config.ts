10857e4f252bd6b8112fcfc8c6542e7f4a509ac0
Charles <chaolinjin@gmail.com>
2025-01-06 17:29:32 +0800

    feat: shared build (#19)

--- packages/shared/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup'
-
-export default defineConfig({
-  entry: ['utils/index.ts', 'types/index.ts', 'configs/*.ts'],
-  clean: true,
-  dts: true,
-  outDir: 'dist',
-  format: ['cjs', 'esm']
-})
